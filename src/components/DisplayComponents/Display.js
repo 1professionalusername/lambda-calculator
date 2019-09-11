13206b2b76377a4fce7107d3d1bccf46cada4af7
Brandon Green <jailhousejordans@gmail.com>
2019-09-11 15:15:51 -0500

    more kinks

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -4,7 +4,7 @@ const Display = () => {
 
   return <div>{/* Display any props data here */}</div>
   return (
-    <input className='calculatorScreen' value='0' type='text' disabled></input>
+    <div></div>
   )
 
 
